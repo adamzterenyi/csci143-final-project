f2d0a60c065494bde51c3ece1d078ab0dadb1b9d
adamzterenyi <adamzterenyi@gmail.com>
2024-05-05 02:03:49 -0700

    pls

--- load_data.sh
+++ load_data.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]; then
-  echo "Usage: $0 --db <database_connection_string> [--user_rows <number_of_users>]"
-  exit 1
-fi
-
 python3 load_data.py --db = "postgresql://postgres:pass@localhost:1362" --user_rows = 100
